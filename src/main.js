a1635ece631d8b82064f5ac7e3bd1eda8cad3513
郭同学 <5667711+guo753951@user.noreply.gitee.com>
2020-11-04 16:21:50 +0800

    登录验证

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from '@/App.vue'
-import router from '@/router'
+import axios from 'axios' // axios
+import router from '@/router' // 路由
 import store from '@/store'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui' // element 组件库
+import 'element-ui/lib/theme-chalk/index.css' // element 组件库
 
 Vue.use(ElementUI)
+Vue.prototype.$http = axios // 挂载 $http 对象 就是axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,31 @@
 <!--  -->
 <template>
-  <div class='contation'>login</div>
+  <!-- 全凭容器 -->
+  <div class='contation-login'>
+    <el-card class="my-card">
+      <img src="../../assets/images/logo.png" alt="">
+
+      <el-form ref="loginForm" status-icon :rules="LoginRules" :model="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+          <el-button>获取验证码</el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-checkbox :value="true">我以阅读用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+
+        <el-button type="primary" style="width:100%" @click="submitForm('loginForm')">立即登录</el-button>
+
+      </el-form>
+
+    </el-card>
+
+  </div>
 </template>
 
 <script>
@@ -12,8 +37,34 @@ export default {
   // import引入的组件需要注入到对象中才能使用
   components: {},
   data () {
+    // 手机号自定义校验
+    var checkMobile = (rule, value, callback) => {
+      var myreg = /^[1][3,4,5,7,8][0-9]{9}$/
+      if (!myreg.test(value)) {
+        callback(new Error('手机号格式错误'))
+      } else {
+        callback()
+      }
+    }
     // 这里存放数据
     return {
+      // 表单数据
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      // 表单进行验证
+      LoginRules: {
+        mobile: [
+          //  是否必填         错误提示                  校验方式
+          { required: true, message: '请输入手机号码', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码6个字符', trigger: 'blur' }
+        ]
+      }
 
     }
   },
@@ -23,6 +74,21 @@ export default {
   watch: {},
   // 方法集合
   methods: {
+
+    submitForm (loginForm) {
+      // 点击登录验证
+      this.$refs[loginForm].validate((valid) => {
+        if (valid) {
+          this.$http.post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm).then(res => {
+            // 登陆成功跳转到首页
+            this.$router.push('/')
+          }).catch(() => {
+            // 错误消息提示
+            this.$message({ showClose: true, message: '手机号输入有误', type: 'error' })
+          })
+        }
+      })
+    }
 
   },
   // 生命周期 - 创建完成（可以访问当前this实例）
@@ -44,4 +110,26 @@ export default {
 </script>
 <style lang='less' scoped>
 // @import url(); 引入公共css类
+.contation-login {
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login-bgc.jpg) no-repeat center / 100%
+    100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  .my-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    img {
+      width: 250px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+  }
+}
 </style>
