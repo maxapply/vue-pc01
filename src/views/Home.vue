925c22117d62669ba8afe855ffdf51716a93a16c
郭同学 <5667711+guo753951@user.noreply.gitee.com>
2020-11-04 09:18:32 +0800

    初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-tag>标签一</el-tag>
+    <i class="el-icon-star-on"></i>
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
